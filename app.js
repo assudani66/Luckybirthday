186ade1c29300e04d016ee986469f5601dc9229b
Phantomcat20 <63794264+assudani66@users.noreply.github.com>
2021-10-01 17:57:00 +0530

    Html done

---
+++ app.js
@@ -0,0 +1,7 @@
+const dateinput = document.querySelector("#date-input")
+const numberinput = document.querySelector("#number-input")
+const checkbutton = document.querySelector("#check-button")
+
+checkbutton.addEventListener("click", function luckanalyzer(){
+    
+})
